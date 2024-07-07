9cbc778e109be506697e3f937eb5463700a454a7
SwaroopDangal <swaroopdangal732@gmail.com>
2024-07-07 09:17:01 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hellllo world");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("server is running" + 3000);
+});
